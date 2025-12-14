d5312d99856a8c13675eaee33aab0145432bdae0
tetrahOh <xtetrah@gmail.com>
2025-12-14 22:37:45 +1100

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById("app").innerHTML = `
+  <h1>🎄 KONG Christmas Games 🎄</h1>
+  <p>Share this link so nobody argues about the rules 😄</p>
+
+  <h2>🎮 Games</h2>
+  <ul>
+    <li>🎈 Ho Ho Blow</li>
+    <li>❄️ Snowball Fight</li>
+    <li>🔔 Jingle Balls</li>
+    <li>😈 Grinch Balloons</li>
+    <li>🧝 Elf Reflex Rumble</li>
+    <li>🦌 Rudolph’s Antlers</li>
+    <li>🧸 Teddy Bear Voice</li>
+  </ul>
+`;
